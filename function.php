9192e7257477e3bcc51a41513cec674143fa6235
anupsamantaray <anup.bapu@gmail.com>
2015-09-01 03:48:17 +0530

    commit

--- function.php
+++ function.php
@@ -9,5 +9,5 @@ catch(Exception $e)
 }
 	//$con=mysql_connect("localhost","learnsci_twcta","hHeOhbPMtR&F") or die(mysql_error());
 	$con=mysql_connect("localhost","root","") or die(mysql_error());
-$db=mysql_select_db("learnsci_kriti",$con)or die(mysql_error());
+	$db=mysql_select_db("learnsci_kriti",$con)or die(mysql_error());
 ?>
