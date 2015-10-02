9de7f3d3f4addc985464374450b13c966e514a85
anupsamantaray <anup.bapu@gmail.com>
2015-10-03 00:07:30 +0530

    jhjh

--- courses/getvideo.php
+++ courses/getvideo.php
@@ -18,70 +18,56 @@ if($numsub>0){
 		if($numvd>0){
 			while($getrestopic=mysql_fetch_array($gettopic)){
 	?>	
-			<h4 style="float:left;">
-				<?php echo $getrestopic['topic'];?>
-				<!--Algebra-->
-			</h4>
-			<div style="width:100%; height:auto; float:left;">
-				<?php
-				$getvideo=mysql_query("select * from `extra_detail` where `class_id`='$classid' and `subject_id`='$getressubject[id]' and `topic_id`='$getrestopic[id]' and `video`!=''");
-				$numvideo=mysql_num_rows($getvideo);
-				if($numvideo>0){
-					while($resvideo=mysql_fetch_array($getvideo)){
-						$i++;
-						if($i==1){
-							?>
-							<div class="videobox" style="margin-left:0px;">
-								<!--<object width="230" height="200">
-								<param name="movie" value="../admin/<?php echo $resvideo['video'];?>"></param>
-								<param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param>
-								<embed src="../admin/<?php echo $resvideo['video'];?>" type="application/x-shockwave-flash" width="230" height="200" allowscriptaccess="always" allowfullscreen="true"></embed>
-								</object>-->
-								<a href="#" class="big-link" data-reveal-id="myModal" data-animation="fade">
-									<img src="images/play.jpg" onclick="play('<?php echo $resvideo['id'];?>')" style="width:100%; float:left;">
-								</a>
-							</div>
-							<?php
-						}
-						elseif($i%4==0){
-						?>
-							<div class="videobox" style="margin-left:0px;">
-								<!-- <object width="230" height="200">
-								<param name="movie" value="../admin/<?php echo $resvideo['video'];?>"></param>
-								<param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param>
-								<embed src="../admin/<?php echo $resvideo['video'];?>" type="application/x-shockwave-flash" width="230" height="200" allowscriptaccess="always" allowfullscreen="true"></embed>
-								</object>-->
-								<a href="#" class="big-link" data-reveal-id="myModal" data-animation="fade">
-									<img src="images/play.jpg" onclick="play('<?php echo $resvideo['id'];?>')" style="width:100%; float:left;">
-								</a>
-							</div>
-							<?php
+				<div class="videonew">
+					<h4 style="float:left;">
+						<?php echo $getrestopic['topic'];?>
+						<!--Algebra-->
+					</h4>
+					<div style="width:100%; height:auto; float:left;">
+						<?php
+						$getvideo=mysql_query("select * from `extra_detail` where `class_id`='$classid' and `subject_id`='$getressubject[id]' and `topic_id`='$getrestopic[id]' and `video`!=''");
+						$numvideo=mysql_num_rows($getvideo);
+						if($numvideo>0){
+							while($resvideo=mysql_fetch_array($getvideo)){
+								$i++;
+								if($i==1){
+									?>
+									<div class="videobox" style="margin-left:0px;">
+										<a href="#" class="big-link" data-reveal-id="myModal" data-animation="fade">
+											<img src="images/play.jpg" onclick="play('<?php echo $resvideo['id'];?>')" style="width:100%; float:left;">
+										</a>
+									</div>
+									<?php
+								}
+								elseif($i%4==0){
+								?>
+									<div class="videobox" style="margin-left:0px;">
+										<a href="#" class="big-link" data-reveal-id="myModal" data-animation="fade">
+											<img src="images/play.jpg" onclick="play('<?php echo $resvideo['id'];?>')" style="width:100%; float:left;">
+										</a>
+									</div>
+									<?php
+								}
+								else{
+								?>
+									<div class="videobox">
+										<a href="#" class="big-link" data-reveal-id="myModal" data-animation="fade">
+											<img src="images/play.jpg" onclick="play('<?php echo $resvideo['id'];?>')" style="width:100%; float:left;">
+										</a>
+									</div>
+									<?php
+								}
+							}
 						}
 						else{
 						?>
-							<div class="videobox">
-								<!-- <object width="230" height="200">
-								 <param name="movie" value="../admin/<?php echo $resvideo['video'];?>"></param>
-								 <param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param>
-								 <embed src="../admin/<?php echo $resvideo['video'];?>" type="application/x-shockwave-flash" width="230" height="200" allowscriptaccess="always" allowfullscreen="true"></embed>
-								 </object>-->
-								<a href="#" class="big-link" data-reveal-id="myModal" data-animation="fade">
-									<img src="images/play.jpg" onclick="play('<?php echo $resvideo['id'];?>')" style="width:100%; float:left;">
-								</a>
-							</div>
-							<?php
+							<h4 style="color: red; font-size: 14px;">Video Comming Soon</h4>
+						<?php
 						}
-					}
-				}
-				else{
-				?>
-					<h4 style="color: red; font-size: 14px;">Video Comming Soon</h4>
+						?>
+					</div>	
+				</div>					
 				<?php
-				}
-				?>
-			</div>	
-								
-		<?php
 			}
 		}else{
 		?>
@@ -95,4 +81,4 @@ else{
 	<h4>There are no records.</h4>
 <?php
 }
-?>							
+?>				
